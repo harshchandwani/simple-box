e4273fbdb6d8d090f3c9a25cfdf43eaaaa5fe0d1
Harsh Chandwani <harshchandwani50028@gmail.com>
2023-09-11 23:49:11 +0530

    Added Js logic

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 var rect = document.querySelector("#center");
 rect
-.addEventListener("mousemove", () => {
-    console.log("mouse is on baby")
+.addEventListener("mousemove", function (event) {
+    var recLocation = rect.getBoundingClientRect();
+    var insideRectVal = event.clientX - recLocation.left;
+    // console.log(recLocation.width);
+    // console.log(event.clientX - recLocation.left);
+    
+    if(insideRectVal <= recLocation.width/2){
+        rect.style.backgroundColor = "pink";
+        var redColor = gsap.utils.mapRange(0, 250, 255, 0, 23)
+        console.log("left");
+        gsap.to(rect, {
+            color: ``
+        })
+    }
+    else{
+        rect.style.backgroundColor = "red";
+        console.log("right");
+    }
 });
