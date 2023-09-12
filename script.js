c55f4eb5800008ec20c02263888f230d7b7a4870
Harsh Chandwani <harshchandwani50028@gmail.com>
2023-09-12 08:22:23 +0530

    Added red and Blue color with opacity

--- script.js
+++ script.js
@@ -7,15 +7,22 @@ rect
     // console.log(event.clientX - recLocation.left);
     
     if(insideRectVal <= recLocation.width/2){
-        rect.style.backgroundColor = "pink";
-        var redColor = gsap.utils.mapRange(0, 250, 255, 0, 23)
-        console.log("left");
+        // rect.style.backgroundColor = "pink";
+        var redColor = gsap.utils.mapRange(0, recLocation.width/2, 255, 0, insideRectVal);
+        // console.log("left");
         gsap.to(rect, {
-            color: ``
+            backgroundColor: `rgb(${redColor},0,0)`,
+            ease: Power4,
         })
     }
     else{
-        rect.style.backgroundColor = "red";
+        // rect.style.backgroundColor = "red";
         console.log("right");
+        var blueColor = gsap.utils.mapRange(recLocation.width/2, recLocation.width, 0, 255, insideRectVal);
+        // console.log("left");
+        gsap.to(rect, {
+            backgroundColor: `rgb(0,0,${blueColor})`,
+            ease: Power4,
+        })
     }
 });
